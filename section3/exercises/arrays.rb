017574cfd2bde0bebe18cb70f635deccabd43901
Aether Kerstens <philippekerstens@gmail.com>
2022-03-06 08:36:06 -0800

    Add section 3

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -25,43 +25,50 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+print animals.length
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
-
+puts animals.pop
+puts animals.push "Gorilla"
+print animals
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
-
+puts animals.pop
+puts animals.push "Pangolin"
+print animals
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+puts animals.push "Elephant"
+print animals
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
-
+foods = ["Muffin", "Sushi", "Donut", "Sausage"]
+print foods
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+print foods.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+puts foods.push "Broccoli"
+print foods
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+puts foods.pop
+print foods
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
-
+puts foods.push "Hazelnuts", "Strawberries", "Raspberries"
+print foods
 # YOU DO: Remove the food that is in index position 0.
-
+puts foods.shift
+print foods
 #-------------------
 # PART 3: Where are Arrays used?
 #-------------------
@@ -79,11 +86,12 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: wikipedia
+historical_event = ["date", "historical figures", "event reasons", "sources"]
+# 2: online stores like amazon
+products = ["tops", "bottoms", "hats", "shoes", "books", "accessories"]
+# 3: youtube
+channel = ["home", "videos", "playlists", "community", "channels", "about"]
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,17 +1,19 @@
 ## Section 3 Reflection
 
 1. How different did your workflow feel this week, considering we asked you to follow the Pomodoro technique?
-
+The pomodoro has 100% boosted my efficiency, as it actually reinforces
 2. Regarding the work you did around setting intentions in Step 1 of the Pomodoro technique - how did that go? Were you surprised by anything (did you find yourself way more focused than you realized, more distracted that you thought you'd be, estimating times accurately or totally off, etc)?
-
+It went so so - Pomodoro has greatly helped my focus and pacing myself - but I still run the risk of ignoring Pomodoro's mandated breaks and thus risking burnout which has happened several times. The tasks took both more time and less time then estimated. I was both more focused on my studies but also more distracted during my breaks.
 3. What is a Hash, and how is it different from an Array?
-
+A Hash stores values in an unordered manner but they are paired with keys, whereas the Array stores only values alone, but in an ordered list. Hashes are also to a certain degree like strings within strings.
 4. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
-
+`pet_store = {food_bowls: 5, cages: 3, pet_food: 20}`
 5. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
-
+`puts states["IA"]`
 6. With the same hash above, how would we get all the keys?  How about all the values?
-
+For the keys it would be: `puts states.keys`
+For the values it would be: `puts states.values`
 7. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
-
+We might use a hash in establishing families, with first name paired to a last name - it would be better than an array as the array would be unable to distinguish all the families from each other unlike a hash.
 8. What questions do you still have about hashes?
+It took me a while to realize that `:` and `=>` are both hashes and I briefly confused them with arrays - as well as mistyping `=>` as `>=` which is a greater-or-equal-than. Why are the hashes so complicated/different - is it a carry over from past editions? Why does the Learn ruby switch different formats - yes it helps prepare for the real world but it is confusing without explanation - still though it is appreciated.
